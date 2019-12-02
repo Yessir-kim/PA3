575b960dd51b8aace462a36bb841f294edccb5fc
yeongbin123 <21600108@handong.edu>
2019-12-03 02:32:03 +0900

    add pb equation

--- example/count.c
+++ example/count.c
@@ -4,12 +4,13 @@
 #include <glib.h>
 #include "../include/libstemmer.h"
 
-typedef struct
+typedef struct __triple
 {
-	char key ;
-	double Neg ;
-	double nonNeg ;
-} TRIPLE ; 
+	char *key ;
+	float Neg ;
+	float nonNeg ;
+	struct __triple *next ;
+} triple ; 
 	
 void 
 print_counter (gpointer key, gpointer value, gpointer userdata) 
@@ -17,8 +18,8 @@ print_counter (gpointer key, gpointer value, gpointer userdata)
 	char * t = key ;
 	int * d = value ;
 
-	if(*d > 100 && *d < 1000)  
-		printf("(%s, %d)\n", t, *d) ;
+	//if(*d > 100 && *d < 1000)  
+	printf("(%s, %d)\n", t, *d) ;
 }
 
 int
@@ -100,7 +101,7 @@ int
 main () 
 {
 
-	TRIPLE store ; // structure declare
+	triple * store = (triple *)malloc(sizeof(triple)) ; // structure declare
 
 	struct sb_stemmer * stemmer ;
 
@@ -117,14 +118,21 @@ main ()
 	size_t r ; 
 	size_t n = 0 ;
 	char * word[] = {"has", "had", "her", "who", "them", "been", "their", "our", "were", "are", "did", "she", "too", "an", "am", "as", "at", "be", "by", "do", "im", "or", "us", "we", "was", "they", "this", "and", "in", "is", "me", "my", "on", "of", "so", "what", "where", "when", "how", "will", "it", "if", "there", "that", "pm", "ua"} ;
-	int Wsize = 46;
-	
-
+	int Wsize = 46 ;
+	GList * list = NULL ;	
+	GList * value = NULL ;
+	GList * Nlist = NULL ;
+	GList * Nvalue = NULL ;	
+	GList * Vtemp = NULL ;
+	int k = 50 ; // set delete point
+	int Ncount = 0 ;
+	int NONcount = 0 ;	
 
 	while (getline(&line, &n, f) >= 0) {
 		char * t ;
 		char * _line = line ;
 		const char * s;
+		Ncount++ ; // Message count
 		// Tokeniztion part		
 		for (t = strtok(line, " \n\t") ; t != 0x0 ; t = strtok(0x0, " \n\t")) {
 			int * d ;
@@ -168,14 +176,31 @@ main ()
 					*d = *d + 1 ; // exist word -> count +1
 				}
 			}
+		
 		}
 		free(_line) ;
 		line = 0x0 ;
 	}
 
-	g_hash_table_foreach(counter, print_counter, 0x0) ;
-	
 	//printf("but: %d\n", *((int *) g_hash_table_lookup(counter, "is"))) ;
+	
+	list = g_hash_table_get_keys(counter) ;
+	
+	value = g_hash_table_get_values(counter) ;
+ 
+	Vtemp = value ;
+
+	for(GList *Ltemp = list ; Ltemp != NULL ; Ltemp = Ltemp->next)
+	{
+		int * cc = Vtemp->data ;
+
+		if(*cc < k)
+			g_hash_table_remove(counter, Ltemp->data) ;
+
+		Vtemp = Vtemp->next ;
+	}
+
+	g_hash_table_foreach(counter, print_counter, 0x0) ;		 
 
 	fclose(f) ;
 
@@ -188,6 +213,7 @@ main ()
                 char * t ;
                 char * _line = line ;
 		const char * s ;
+		NONcount++ ; // Message count
                 // Tokeniztion part
                 for (t = strtok(line, " \n\t") ; t != 0x0 ; t = strtok(0x0, " \n\t")) {
                         int * d ;
@@ -234,12 +260,85 @@ main ()
                	line = 0x0 ;
        	}
        	printf("\n") ;
+	
+	Nlist = g_hash_table_get_keys(Ncounter) ;
+
+        Nvalue = g_hash_table_get_values(Ncounter) ;
+
+        Vtemp = Nvalue ;
+
+        for(GList *Ltemp = Nlist ; Ltemp != NULL ; Ltemp = Ltemp->next)
+        {
+                int * cc = Vtemp->data ;
+
+                if(*cc < k) 
+                        g_hash_table_remove(Ncounter, Ltemp->data) ;
+
+                Vtemp = Vtemp->next ;
+        }
+
        	g_hash_table_foreach(Ncounter, print_counter, 0x0) ;
 
        	//printf("but: %d\n", *((int *) g_hash_table_lookup(Ncounter, "servic"))) ;
        	fclose(fp) ;
+
 	sb_stemmer_delete(stemmer) ;
 	
-	// probablity cal part
+	/*
+	probablity cal part using GList variable
+	1. list, value // Nlist, Nvalue
+	2. store is Linked list 
+	*/	
+	int num = 0; // smoothing value 
+	float resize = Ncount * 1.0 / NONcount ; // fit rate 
+	/*
+	printf("%f ",resize) ;
+	printf("%d ", Ncount) ;
+	printf("%d", NONcount) ;
+	*/	
+	// Standard : negative count
+	triple *temp = store ;
+	list  = g_hash_table_get_keys(counter) ;
+	Nlist = g_hash_table_get_keys(Ncounter) ;
 	
+	for(GList *Ltemp = list; Ltemp != NULL;  Ltemp = Ltemp->next)
+	{
+		char * negative = Ltemp->data ; // one key
+		int *Vnegative = (int*)g_hash_table_lookup(counter, Ltemp->data) ;
+		GList * Ntemp = Nlist ;
+		while(Ntemp != NULL)
+		{
+			if(strcmp(negative,Ntemp->data) == 0)				{
+				int *VNnegative = (int*)g_hash_table_lookup(Ncounter, Ntemp->data) ;
+				store->key = Ltemp->data ;
+				//printf("%s ", store->key) ;
+				store->Neg = (num + *Vnegative)*1.0 / (2*num + Ncount) ;
+				//printf("%f ",store->Neg) ;
+				store->nonNeg = (num + *VNnegative)*1.0 / (2*num + Ncount) ;
+				//store->nonNeg *= resize ;
+				//printf("%f\n",store->nonNeg) ;
+				store->next = (triple *)malloc(sizeof(triple)) ;
+				store = store->next ;
+				break;
+			} 
+			Ntemp = Ntemp->next ;
+		}
+		if(Ntemp == NULL)
+		{
+				int VNnegative = 0 ;
+				store->key = Ltemp->data ;
+				//printf("%s ", store->key) ;
+				store->Neg = (num + *Vnegative)*1.0 / (2*num + Ncount) ;			
+				//printf("%f ",store->Neg) ;
+				store->nonNeg = (num + VNnegative)*1.0 / (2*num + Ncount) ;
+				//store->nonNeg *= resize ;
+				//printf("%f\n",store->nonNeg) ;	
+				store->next = (triple *)malloc(sizeof(triple)) ;
+				store = store->next ;
+		}
+	}
+	store->next = NULL ; // set end point 
+				
+	for(; temp->next != NULL ; temp = temp->next)
+		printf("%s   %.2f    %.2f\n",temp->key,temp->Neg,temp->nonNeg) ;
 }
